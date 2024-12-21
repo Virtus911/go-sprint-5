5a77a3f5a374affcaf314727d4db378519844972
Sergey <zaharov72729@gmail.com>
2024-12-22 00:12:44 +0300

    iteration2

--- ftracker.go
+++ ftracker.go
@@ -48,6 +48,7 @@ func meanSpeed(action int, duration float64) float64 {
 // countPool int — сколько раз пользователь переплыл бассейн.
 func ShowTrainingInfo(action int, trainingType string, duration, weight, height float64, lengthPool, countPool int) string {
 	// ваш код здесь
+	height = height / cmInM
 	switch {
 	case trainingType == "Бег":
 		distance := distance(action)                               // вызовите здесь необходимую функцию
@@ -55,12 +56,14 @@ func ShowTrainingInfo(action int, trainingType string, duration, weight, height 
 		calories := RunningSpentCalories(action, weight, duration) // вызовите здесь необходимую функцию
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\n"+
 			"Скорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
+
 	case trainingType == "Ходьба":
 		distance := distance(action)                                       // вызовите здесь необходимую функцию
 		speed := meanSpeed(action, duration)                               // вызовите здесь необходимую функцию
 		calories := WalkingSpentCalories(action, weight, duration, height) // вызовите здесь необходимую функцию
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\n"+
 			"Скорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
+
 	case trainingType == "Плавание":
 		distance := float64(lengthPool*countPool) / mInKm                          // вызовите здесь необходимую функцию
 		speed := swimmingMeanSpeed(lengthPool, countPool, duration)                // вызовите здесь необходимую функцию
@@ -109,8 +112,8 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	meanSpeed := meanSpeed(action, duration)
-	caloriesPerHour := ((walkingCaloriesWeightMultiplier*weight + (math.Pow(meanSpeed, 2)/height)*
-		walkingSpeedHeightMultiplier*weight) * duration * minInH)
+	caloriesPerHour := ((walkingCaloriesWeightMultiplier*weight +
+		(math.Pow(meanSpeed, 2)/height)*walkingSpeedHeightMultiplier*weight) * duration * minInH)
 
 	return caloriesPerHour
 }
